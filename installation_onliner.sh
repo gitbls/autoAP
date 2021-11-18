cde555e90d86ae03662861f4cfbbdc088ee68d53
Patrick McGuire <mcguirepr89@gmail.com>
2021-11-18 11:01:17 -0500

    adding installation one-liner

---
+++ installation_onliner.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+sudo curl -L https://github.com/gitbls/autoAP/raw/master/autoAP.sh -o /usr/local/bin/autoAP.sh
+sudo curl -L https://github.com/gitbls/autoAP/raw/master/install-autoAP -o /usr/local/bin/install-autoAP
+sudo curl -L https://github.com/gitbls/autoAP/raw/master/rpi-networkconfig -o /usr/local/bin/rpi-networkconfig
+sudo chmod 755 /usr/local/bin/autoAP.sh /usr/local/bin/install-autoAP /usr/local/bin/rpi-networkconfig
+sudo /usr/local/bin/install-autoAP
